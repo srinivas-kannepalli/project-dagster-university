ffa83930a383820ce3d64b2486130971707c9fab
srinivas-kannepalli <151536666+srinivas-kannepalli@users.noreply.github.com>
2025-07-18 06:01:05 +0000

    add asset checks

--- dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
+++ dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
@@ -1,6 +1,7 @@
 import dagster as dg
 from pathlib import Path
 from dagster_duckdb import DuckDBResource
+import csv
 
 class IngestionFileConfig(dg.Config):
     path: str
@@ -35,3 +36,26 @@ def duckdb_table(
         conn.execute(table_query)
         conn.execute(f"copy {table_name} from '{import_file}';")
 
+@dg.asset_check(
+    asset=import_file,
+    blocking=True,
+    description="Ensure file contains no zero value shares",
+)
+def not_empty(
+    context: dg.AssetCheckExecutionContext,
+    import_file,
+) -> dg.AssetCheckResult:
+    with open(import_file, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        data = (row for row in reader)
+
+        for row in data:
+            if float(row["share_price"]) <= 0:
+                return dg.AssetCheckResult(
+                    passed=False,
+                    metadata={"'share' is below 0": row},
+                )
+
+    return dg.AssetCheckResult(
+        passed=True,
+    )
